5ead51ddc270b3941c9e988a6f4b3a468b4ea844
Tony DiCola <tony@tonydicola.com>
2014-06-06 22:01:04 -0700

    Initial commit.

---
+++ Adafruit_DHT/platform_detect.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import platform
+
+# Platform identification constants.
+UNKNOWN          = 0
+RASPBERRY_PI     = 1
+BEAGLEBONE_BLACK = 2
+
+
+def platform_detect(plat=platform.platform()):
+	"""Detect if running on the Raspberry Pi or Beaglebone Black and return the
+	platform type.  Will return RASPBERRY_PI, BEAGLEBONE_BLACK, or UNKNOWN."""
+	# Handle Raspberry Pi
+	# Platform output on Raspbian testing/jessie ~May 2014:
+	# Linux-3.10.25+-armv6l-with-debian-7.4
+	if plat.lower().find('armv6l-with-debian') > -1:
+		return RASPBERRY_PI
+	# Handle Beaglebone Black
+	# Platform output on Debian ~May 2014:
+	# Linux-3.8.13-bone47-armv7l-with-debian-7.4
+	elif plat.lower().find('armv7l-with-debian') > -1:
+		return BEAGLEBONE_BLACK
+	else:
+		return UNKNOWN
---
+++ setup.py
@@ -0,0 +1,55 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages, Extension
+import sys
+
+import Adafruit_DHT.platform_detect as platform_detect
+
+
+# Check if an explicit platform was chosen with a command line parameter.
+# Kind of hacky to manipulate the argument list before calling setup, but it's
+# the best simple option for adding optional config to the setup.
+platform = platform_detect.UNKNOWN
+if '--force-pi' in sys.argv:
+	platform = platform_detect.RASPBERRY_PI
+	sys.argv.remove('--force-pi')
+elif '--force-bbb' in sys.argv:
+	platform = platform_detect.BEAGLEBONE_BLACK
+	sys.argv.remove('--force-bbb')
+elif '--force-test' in sys.argv:
+	platform = 'TEST'
+	sys.argv.remove('--force-test')
+else:
+	# No explicit platform chosen, detect the current platform.
+	platform = platform_detect.platform_detect()
+
+# Pick the right extension to compile based on the platform.
+extensions = []
+if platform == platform_detect.RASPBERRY_PI:
+	extensions.append(Extension("Adafruit_DHT.Raspberry_Pi_Driver", 
+								["source/_Raspberry_Pi_Driver.c", "source/common_dht_read.c", "source/Raspberry_Pi/pi_dht_read.c"], 
+								libraries=['bcm2835', 'rt'],
+								extra_compile_args=['-std=gnu99']))
+elif platform == platform_detect.BEAGLEBONE_BLACK:
+	extensions.append(Extension("Adafruit_DHT.Beaglebone_Black_Driver",
+								["source/_Beaglebone_Black_Driver.c", "source/common_dht_read.c", "source/Beaglebone_Black/bbb_dht_read.c", "source/Beaglebone_Black/bbb_mmio.c"],
+								libraries=['rt'],
+								extra_compile_args=['-std=gnu99']))
+elif platform == 'TEST':
+	extensions.append(Extension("Adafruit_DHT.Test_Driver",
+								["source/_Test_Driver.c", "source/Test/test_dht_read.c"],
+								extra_compile_args=['-std=gnu99']))
+else:
+	print 'Could not detect if running on the Raspberry Pi or Beaglebone Black.  If this failure is unexpected, you can run again with --force-pi or --force-bbb parameter to force using the Raspberry Pi or Beaglebone Black respectively.'
+	sys.exit(1)
+
+# Call setuptools setup function to install package.
+setup(name              = 'Adafruit_DHT',
+	  version           = '1.0.0',
+	  author            = 'Tony DiCola',
+	  author_email      = 'tdicola@adafruit.com',
+	  description       = 'Library to get readings from the DHT11, DHT22, and AM2302 humidity and temperature sensors on a Raspberry Pi or Beaglebone Black.',
+	  license           = 'MIT',
+	  url               = 'https://github.com/adafruit/Adafruit_Python_DHT/',
+	  packages          = find_packages(),
+	  ext_modules       = extensions)
