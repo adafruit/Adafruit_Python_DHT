2c985346c99fdb67cfa3f1538ff57e84c71fca48
Tony DiCola <tony@tonydicola.com>
2014-06-09 14:08:08 -0700

    Add Adafruit_DHT.py example.

--- examples/Adafruit_DHT.py
+++ examples/Adafruit_DHT.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # Copyright (c) 2014 Adafruit Industries
 # Author: Tony DiCola
 
@@ -18,21 +19,32 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import common
-import Raspberry_Pi_Driver as driver
+import sys
 
-def read(sensor, pin):
-	# Validate pin is a valid GPIO.
-	if pin is None or int(pin) < 0 or int(pin) > 31:
-		raise ValueError('Pin must be a valid GPIO number 0 to 31.')
-	# Get a reading from C driver code.
-	result, humidity, temp = driver.read(sensor, int(pin))
-	if result in common.TRANSIENT_ERRORS:
-		# Signal no result could be obtained, but the caller can retry.
-		return (None, None)
-	elif result == common.DHT_ERROR_GPIO:
-		raise RuntimeError('Error accessing GPIO. Make sure program is run as root with sudo!')
-	elif result != common.DHT_SUCCESS:
-		# Some kind of error occured.
-		raise RuntimeError('Error calling DHT test driver read: {0}'.format(result))
-	return (humidity, temp)
+import Adafruit_DHT
+
+
+# Parse command line parameters.
+sensor_args = { '11': Adafruit_DHT.DHT11,
+				'22': Adafruit_DHT.DHT22,
+				'2302': Adafruit_DHT.AM2302 }
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+	sensor = sensor_args[sys.argv[1]]
+	pin = sys.argv[2]
+else:
+	print 'usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#'
+	print 'example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4'
+	sys.exit(1)
+
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+# Note that sometimes you won't get a reading and
+# the results will be null (because Linux can't
+# guarantee the timing of calls to read the sensor).  
+# If this happens try again!
+if humidity is not None and temperature is not None:
+	print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+else:
+	print 'Failed to get reading. Try again!'
