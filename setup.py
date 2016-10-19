3f30544303b4f2563c7bc6217001d15cc9ea9a5e
Tony DiCola <tony@tonydicola.com>
2016-10-19 22:15:00 +0000

    Fix python3 pip install issue with ez_setup relative import.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from ez_setup import use_setuptools
-use_setuptools()
+try:
+    # Try using ez_setup to install setuptools if not already installed.
+    from ez_setup import use_setuptools
+    use_setuptools()
+except ImportError:
+    # Ignore import error and assume Python 3 which already has setuptools.
+    pass
+
 from setuptools import setup, find_packages, Extension
 import sys
 
@@ -72,7 +78,7 @@ classifiers = ['Development Status :: 4 - Beta',
 
 # Call setuptools setup function to install package.
 setup(name              = 'Adafruit_DHT',
-      version           = '1.3.0',
+      version           = '1.3.1',
       author            = 'Tony DiCola',
       author_email      = 'tdicola@adafruit.com',
       description       = 'Library to get readings from the DHT11, DHT22, and AM2302 humidity and temperature sensors on a Raspberry Pi or Beaglebone Black.',
