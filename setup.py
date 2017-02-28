da8cddf7fb629c1ef4f046ca44f42523c9cf2d11
Tony DiCola <tony@tonydicola.com>
2017-02-28 23:35:34 +0000

    Fix to setup.py for pi 3 detection from previous pull.

--- setup.py
+++ setup.py
@@ -52,6 +52,11 @@ if platform == platform_detect.RASPBERRY_PI:
                                     ["source/_Raspberry_Pi_2_Driver.c", "source/common_dht_read.c", "source/Raspberry_Pi_2/pi_2_dht_read.c", "source/Raspberry_Pi_2/pi_2_mmio.c"],
                                     libraries=['rt'],
                                     extra_compile_args=['-std=gnu99']))
+    elif pi_version == 3:
+        extensions.append(Extension("Adafruit_DHT.Raspberry_Pi_2_Driver",
+                                    ["source/_Raspberry_Pi_2_Driver.c", "source/common_dht_read.c", "source/Raspberry_Pi_2/pi_2_dht_read.c", "source/Raspberry_Pi_2/pi_2_mmio.c"],
+                                    libraries=['rt'],
+                                    extra_compile_args=['-std=gnu99']))
     else:
         raise RuntimeError('Detected Pi version that has no appropriate driver available.')
 elif platform == platform_detect.BEAGLEBONE_BLACK:
