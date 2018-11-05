d8bceec7b5a6fe77e6a6363c14b23b5e98f50a2d
Davide Permunian <davide@davidepermunian.com>
2018-11-05 21:12:14 +0100

    Fix long cable timeout also on RPI2

--- source/Raspberry_Pi_2/pi_2_dht_read.c
+++ source/Raspberry_Pi_2/pi_2_dht_read.c
@@ -72,7 +72,9 @@ int pi_2_dht_read(int type, int pin, float* humidity, float* temperature) {
   // Set pin at input.
   pi_2_mmio_set_input(pin);
   // Need a very short delay before reading pins or else value is sometimes still low.
-  for (volatile int i = 0; i < 50; ++i) {
+  
+  //FIX LONG CABLES ON RPI2 - dade80vr
+  for (volatile int i = 0; i < 500; ++i) {
   }
 
   // Wait for DHT to pull pin low.
