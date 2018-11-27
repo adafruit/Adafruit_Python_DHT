774cbfd66c187d9fe28673112e759fcb3776e811
adj3468 <45388451+adj3468@users.noreply.github.com>
2018-11-27 19:55:55 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -47,8 +47,11 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
+while True:
+  humidity, temperature = Adafruit_DHT.read_retry(sensor,4)
+
+  if humidity is not None and temperature is not None:
+      print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+  else:
+      print('Failed to get reading. Try again!')
+      sys.exit(1)
